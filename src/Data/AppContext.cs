c41e58fd394f5f508a19a3ce73bc52f1cec08bb5
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-07-20 23:53:13 -0300

    Add AppContext

---
+++ src/Data/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using AlkemyChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkemyChallenge.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
